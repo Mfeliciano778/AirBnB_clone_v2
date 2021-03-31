ae317c4c16ac96aa6e33054ce4ec1b341c99c343
Mfeliciano778 <1652@holbertonschool.com>
2021-03-31 19:02:53 +0000

    All task 10

--- models/place.py
+++ models/place.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
+from os import environ, get_terminal_size
 
+
+place_amenity = Table('place_amenity', Base.metadata,
+                    Column('place_id', String(60),
+                            ForeignKey('places.id'),
+                            primary_key=True, nullable=False),
+                    Column('amenity_id', String(60),
+                            ForeignKey('amenities.id'),
+                            primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -19,3 +28,21 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship('Review', cascade='all, delete', backref='place')
+        amenities = relationship('Amenity', backref='place_amenities',
+                                    secondary='place_amenity',
+                                    viewonly=False)
+    else:
+        @property
+        def amenities(self):
+            '''amenities getter'''
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            '''amenities setter'''
+            if type(obj).__name__ == 'Amenity':
+                self.amenity_ids.append(obj)
+            
