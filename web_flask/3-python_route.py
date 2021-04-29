8609b0a9eb3ec5d955820924cf3e043e2d5191cc
Mfeliciano778 <1652@holbertonschool.com>
2021-04-29 18:38:56 +0000

    Task 3, complete??

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''starts flask'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    '''hello hbnb'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def start():
+    '''hello hbnb'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def print_t(text):
+    '''hello hbnb'''
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def print_text(text='is cool'):
+    '''hello hbnb'''
+    return "Python {}".format(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
