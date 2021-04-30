14112b16b19ccc8f22e810cec83cb29511b82145
Mfeliciano778 <1652@holbertonschool.com>
2021-04-30 20:37:15 +0000

    Fixed some stuff in 8 and completed 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,11 +3,12 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_session(self):
+def teardown(self):
     '''closes session'''
     storage.close()
 
@@ -15,7 +16,10 @@ def close_session(self):
 @app.route('/cities_by_states', strict_slashes=False)
 def html_display():
     allstate = storage.all(State)
-    return render_template('7-states_list.html', allstate=allstate)
+    allcities = storage.all(City)
+    return render_template("8-cities_by_states.html", allstate=allstate,
+                            allcities=allcities)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
